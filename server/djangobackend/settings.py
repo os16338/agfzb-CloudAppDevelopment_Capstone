5208bdf8d6db1968d0fc804fb9de768026e33c22
os16338 <os16338@gmail.com>
2022-10-11 15:58:21 -0400

    Add changes from module 1 changes to Github

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost","localhost:8000","bestcars-oars92.us-south.cf.appdomain.cloud"]
 
 
 # Application definition
